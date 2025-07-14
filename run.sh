d7d7d4830614a84c8a19ce83842f76037da3d6cc
tsxc <tsxc_personal@163.com>
2025-07-14 13:48:44 +0800

    fix: 固定依赖版本以修复

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/Light-Beacon/HomepageBuilder
+git clone --branch v0.14.2 --depth=1 https://github.com/Light-Beacon/HomepageBuilder
 pip install ./HomepageBuilder
 pip install -r ./requirements.txt
 python ./script.py
